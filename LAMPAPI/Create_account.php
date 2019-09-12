317b2ed5390f4a9cf94b61df9c055a4d2f9ab953
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-12 11:08:05 -0400

    Add files via upload

--- LAMPAPI/Create_account.php
+++ LAMPAPI/Create_account.php
@@ -3,10 +3,10 @@
 $servername = "localhost";
 $uname = "root";
 $pword = "cop4331";
-$databasename = "test_database";
+$databasename = "database";
 
 // creating connections
-$conn = new mysqli($servername, $uname, $pword, $databasename);
+$conn = new mysqli($servername, $name, $pword, $databasename);
 
 //  check connections
 if(!$conn)
@@ -19,7 +19,7 @@ else
   echo "Connect succesfully";
 }
 
-$sql = "INSERT INTO Login (Uname, Pword) VALUES('John TWO', 'BEN IS THE BEST')";
+$sql = "INSERT INTO user_list (first_name, last_name, phone, email, other_info, email_verification) VALUES('Ben', 'clyde', '1234567890', 'awesomesauce@gmail.com', 'null', '0')";
 
 if($conn->query($sql) === TRUE)
   echo "New record created successfully";

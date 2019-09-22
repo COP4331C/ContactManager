fce9f689be0e3a4bcf351efd6ce090d880141548
Clyde <clyde@pop-os.localdomain>
2019-09-22 12:06:04 -0400

    Testing with the default sql string given in phpMyAdmin to work off of

--- LAMPAPI/update.php
+++ LAMPAPI/update.php
@@ -11,25 +11,29 @@ if(!$conn->conn_error)
 
 else
 {
-  $sql = "UPDATE contact_list SET first_name = '" . $inData["first_name"] . "', last_name= '" . $inData["last_name"] . "', phone= '" . $inData["phone"] . "', email= '" . $inData["email"] . "' WHERE contact list . cid = '" . $inData["cid"] . "'";
+	// $sql = "UPDATE contact_list SET first_name = '" . $inData["first_name"] . "', last_name= '" . $inData["last_name"] . "', phone= '" . $inData["phone"] . "', email= '" . $inData["email"] . "' WHERE contact list . cid = '" . $inData["cid"] . "'";
 
-  $result = $conn->query($sql);
-  if($result->num_row > 0)
-  {
-    $row = $result->fetch->fetch_assoc();
-    $cid = $row["cid"];
-    $first_name = $row["first_name"];
-    $last_name = $row["last_name"];
-    $phone = $row["phone"];
-    $email = $row["email"];
+	$sql = "UPDATE `user_list` SET `email` = 'shrek@theswamp.net' WHERE `user_list`.`id` = 3";
 
-    returnWithInfo($id, $first_name, $last_name, $phone, $email);
-  }
-  else
-  {
-    returnWithError('No Records Found');
-  }
-  $conn->close();
+	$result = $conn->query($sql);
+	if($result->num_row > 0)
+	{
+		$row = $result->fetch->fetch_assoc();
+		$cid = $row["cid"];
+		$first_name = $row["first_name"];
+		$last_name = $row["last_name"];
+		$phone = $row["phone"];
+		$email = $row["email"];
+	
+		returnWithInfo($id, $first_name, $last_name, $phone, $email);
+	}
+	else
+	{
+		returnWithError('No Records Found');
+	}
+
+$conn->close();
+
 }
 
 function getRequestInfo()

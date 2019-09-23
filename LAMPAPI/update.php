b62c2c679bde5216702cb7eaf96fe3bf526fc907
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-23 15:30:06 -0400

    Update update.php

--- LAMPAPI/update.php
+++ LAMPAPI/update.php
@@ -12,7 +12,7 @@ if($conn->connect_error)
 else
 {
 	$cid = $inData["cid"];
-	$sql = "UPDATE contact_list SET first_name = '" . $inData["first_name"] . "', last_name= '" . $inData["last_name"] . "', phone= '" . $inData["phone"] . "', email= '" . $inData["email"] . "' WHERE contact list . cid = '" . $inData["cid"] . "'";
+	$sql = "UPDATE contact_list SET first_name = '" . $inData["first_name"] . "', last_name= '" . $inData["last_name"] . "', phone= '" . $inData["phone"] . "', email= '" . $inData["email"] . "' WHERE cid = '" . $inData["cid"] . "'";
 	returnWithError($sql);
 	// Uncommen to test just a basic string that i pulled from phpMyAdmin
 	//$sql = "UPDATE `contact_list` SET `email` = 'donkey@classtraitor.net' WHERE `contact_list`.`cid` = 2";

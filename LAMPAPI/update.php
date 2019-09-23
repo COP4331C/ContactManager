700f5303f52017700b6555bec84450fcadcfe24a
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-23 15:35:14 -0400

    Update update.php

--- LAMPAPI/update.php
+++ LAMPAPI/update.php
@@ -13,7 +13,7 @@ else
 {
 	$cid = $inData["cid"];
 	$sql = "UPDATE contact_list SET first_name = '" . $inData["first_name"] . "', last_name= '" . $inData["last_name"] . "', phone= '" . $inData["phone"] . "', email= '" . $inData["email"] . "' WHERE cid = '" . $inData["cid"] . "'";
-	returnWithError($sql);
+	//returnWithError($sql);
 	// Uncommen to test just a basic string that i pulled from phpMyAdmin
 	//$sql = "UPDATE `contact_list` SET `email` = 'donkey@classtraitor.net' WHERE `contact_list`.`cid` = 2";
 

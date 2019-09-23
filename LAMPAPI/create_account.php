9d7cbb38a41895c7d34ef1184edac344f8331b0b
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-23 19:48:38 -0400

    Update create_account.php

--- LAMPAPI/create_account.php
+++ LAMPAPI/create_account.php
@@ -1,70 +1,5 @@
 <?php
 
-/*$inData = getRequestInfo();
-
-$servername = "localhost";
-$uname = "root";
-$pword = "cop4331";
-$databasename = "database";
-
-// creating connections
-$conn = new mysqli($servername, $uname, $pword, $databasename);
-
-//  check connections
-if(!$conn)
-{
-  die("Connection failed: ". $conn->connect_error);
-}
-
-else
-{
-  echo "Connect succesfully";
-}
-
-$sql = "INSERT INTO user_list (email, pass, first_name, last_name, phone, email) VALUES($inData["email"]', inData["pass"], $inData["first_name"]', '$inData["last_name"]', '$inData["phone"]', '$inData["email"]')";
-$result = $conn->query($sql);
-if($result->num_row <= 0)
-{
-    $row = $result->fetch->fetch_assoc();
-    $email = $row["email"];
-    $pass = $row["pass"]
-    $first_name = $row["first_name"];
-    $last_name = $row["last_name"];
-    $phone = $row["phone"];
-    $email = $row["email"];
-
-    returnWithInfo($email, $pass, $first_name, $last_name, $phone, $email);
- 
-}
-else
-{
-    returnWithError( "Contact Failed to be added" );
-}
-$conn->close();
-
-function getRequestInfo()
-{
-		return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson( $obj )
-{
-		header('Content-type: application/json');
-		echo $obj;
-}
-
-function returnWithError( $err )
-{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-}
-
-function returnWithInfo($email, $pass, $first_name, $last_name, $phone, $email);
-{
-	$retValue = '{"email":"' . $email . '","pass":"' . $pass . '","first_name":"' . $first_name . '","last_name":"' . $last_name . '","phone":"'. $phone . '","email":"' . $email .'","message":"Contact has been added!"}';
-	sendResultInfoAsJson( $retValue );
-}*/
-
 $inData = getRequestInfo();
 
 $servername = "localhost";
@@ -83,30 +18,33 @@ if(!$conn->conn_error)
 
 else
 {
-//$sql = "INSERT INTO user_list (first_name, last_name, phone, email, other_info, email_verification) VALUES('Ben', 'clyde', '1234567890', 'awesomesauce@gmail.com', 'null', '0')";
+	//$sql = "INSERT INTO user_list (first_name, last_name, phone, email, other_info, email_verification) VALUES('Ben', 'clyde', '1234567890', 'awesomesauce@gmail.com', 'null', '0')";
 
-$sql = "INSERT INTO `user_list` (first_name, last_name, phone, email, pass) VALUES('" . $inData["first_name"] . "', '" . $inData["last_name"] . "', '" . $inData["phone"] . "', '" . $inData["email"]. "')";
+	$sql = "INSERT INTO `user_list` (first_name, last_name, phone, email, pass) VALUES('" . $inData["first_name"] . "', '" . $inData["last_name"] . "', '" . $inData["phone"] . "', '" . $inData["email"]. "')";
 
-if($conn->query($sql) === TRUE)
-{
-  echo "New record created successfully";
+	if($conn->query($sql) === TRUE)
+	{
+  	 echo "New record created successfully";
 
-  //$row = $result->fetch->fetch_assoc();
-  //$first_name = $row["first_name"];
-  $first_name = $inData["first_name"];
-  //$last_name = $row["last_name"];
-  $last_name = $inData["last_name"];
-  //$phone = $row["phone"]; 
-  $phone = $inData["phone"];
-  //$email = $row["email"];
-  $email = $inData["email"];
-  $conn->close();
+  	//$row = $result->fetch->fetch_assoc();
+  	//$first_name = $row["first_name"];
+  	$first_name = $inData["first_name"];
+  	//$last_name = $row["last_name"];
+  	$last_name = $inData["last_name"];
+  	//$phone = $row["phone"]; 
+  	$phone = $inData["phone"];
+  	//$email = $row["email"];
+  	$email = $inData["email"];
+  	$conn->close();
 
-  returnWithInfo($first_name, $last_name, $phone, $email);
-}
-else
-{
-  returnWithError("Account Creation Failed!");
+  	returnWithInfo($first_name, $last_name, $phone, $email);
+	}
+	
+	else
+	{
+  	returnWithError("Account Creation Failed!");
+	}
+
 }
 
 

f1625601a467c8bb07fb0220140a16839f86b8e7
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-24 16:45:18 -0400

    Update add_contact.php

--- LAMPAPI/add_contact.php
+++ LAMPAPI/add_contact.php
@@ -1,5 +1,7 @@
 <?php
 
+$inData = getRequestInfo();
+
 $servername = "localhost";
 $uname = "root";
 $pword = "cop4331";
@@ -8,23 +10,56 @@ $dbname = "database";
 $conn = new mysqli($servername, $uname, $pword, $dbname);
 
 //  check connections
-if(!$conn)
+if($conn->conn_error)
 {
-  die("Connection failed: ". $conn->connect_error);
+  returnWithError("Connection failed: ");
 }
 
 else
 {
-  echo "Connect succesfully";
+
+  //$sql ="INSERT INTO contact_list (id, first_name, last_name, phone, email) VALUES('34','TRIET', 'BRYAN', 1234567890, 'Toawesomeforthisproject@gmail.com' , 'NULL')";
+  $sql = "INSERT INTO `user_list` (id, first_name, last_name, phone, email) VALUES('" . $inData["id"] . "', '" . $inData["first_name"] . "', '" . $inData["last_name"] . "', '" . $inData["phone"] . "', '" . $inData["email"] .  "')";
+  if($conn->query($sql) === TRUE)
+  {
+    echo "New record created successfully";
+    $id = $inData["id"];
+    $firstName = ["first_name"];
+    $lastName = ["last_name"];
+    $phone = ["phone"];
+    $email = ["email"];
+    $conn->close();
+    
+    returnWithInfo($id, $first_name, $last_name, $phone, $email);
+    
+  }
+  else 
+  {
+      returnWithError("Contact Creation failure");
+  }
+  
+  
+  
 }
 
-$sql ="INSERT INTO contact_list (first_name, last_name, phone, email, other) VALUES('TRIET', 'BRYAN', 1234567890, 'Toawesomeforthisproject@gmail.com' , 'NULL')";
-
-if($conn->query($sql) === TRUE)
-  echo "New record created successfully";
-
-else {
-    echo "Error";
+function getRequestInfo()
+{
+  return json_decode(file_get_contents('php://input'), true);
 }
-$conn->close()
+function sendResultInfoAsJson( $obj )
+{
+  header('Content-type: application/json');
+  echo $obj;
+}
+function returnWithError( $err )
+{
+  $retValue = '{"id":"0","firstName":"","lastName":"","phone":"","email":"","error":"' . $err . '"}';
+  sendResultInfoAsJson( $retValue );
+}
+function returnWithInfo( $id, $firstName, $lastName, $email, $phone)
+{
+  $retValue = '{"id":"'. $id . '","first_name":"' . $firstName . '","last_name":"' . $lastName . '","phone":"'. $phone . '","email":"' . $email . '","error":""}';
+  sendResultInfoAsJson( $retValue );
+}
+
 ?>

ff10ba5363bc8f24001b1249af25eba5be701b3c
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-24 15:48:41 -0400

    Update delete_contact.php

--- LAMPAPI/delete_contact.php
+++ LAMPAPI/delete_contact.php
@@ -22,15 +22,15 @@ else
   //$sql = "DELETE FROM contact_list where cid='" . $inData["cid"] . "' and id='" . $inData["id"] . "'";
   $sql = "DELETE FROM `contact_list` where cid = '". $inData["cid"] . "' and id ='" . $inData["id"] . "'";
   $result = $conn->query($sql);
-  if($result->num_rows > 0)
+  if($conn->query($sql) === True)
   {
     $row = $result->fetch->fetch_assoc();
-    $cid = $row["cid"];
-    $id = $row["id"];
-    $first_name = $row["first_name"];
-    $last_name = $row["last_name"];
-    $phone = $row["phone"];
-    $email = $row["email"];
+    $cid = $inData["cid"];
+    $id = $inData["id"];
+    $first_name = $inData["first_name"];
+    $last_name = $inData["last_name"];
+    $phone = $inData["phone"];
+    $email = $inData["email"];
 
     returnWithInfo($cid,$id, $first_name, $last_name, $phone, $email);
   }

d8498f19e2ff32cfb24c8bdb94d0f153ef3bc6c9
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-09-24 14:19:53 -0400

    Update delete_contact.php

--- LAMPAPI/delete_contact.php
+++ LAMPAPI/delete_contact.php
@@ -19,18 +19,20 @@ else
 //  $sql = "DELETE FROM contact_list where first_name='" . $inData["first_name"] . "'and last_name='" . $inData["last_name"] . "'and phone='" . $inData["phone"] . "'and email='" . $inData["email"] . "'and id='" . $indata["id"] . "'";
   
   // Trying to see if using just id + cid makes it any easier - clyde
-  $sql = "DELETE FROM contact_list where cid='" . $inData["cid"] . "' and id='" . $inData["id"] . "'";
+  //$sql = "DELETE FROM contact_list where cid='" . $inData["cid"] . "' and id='" . $inData["id"] . "'";
+  $sql = "DELETE FROM contact_list where cid = '". $inData["cid"] . "' and id ='" . $inData["id"] . "'";
   $result = $conn->query($sql);
   if($result->num_row > 0)
   {
     $row = $result->fetch->fetch_assoc();
+    $cid = $row["cid"];
     $id = $row["id"];
     $first_name = $row["first_name"];
     $last_name = $row["last_name"];
     $phone = $row["phone"];
     $email = $row["email"];
 
-    returnWithInfo($id, $first_name, $last_name, $phone, $email);
+    returnWithInfo($cid,$id, $first_name, $last_name, $phone, $email);
   }
   else
   {
@@ -40,7 +42,7 @@ else
 
 }
 
-function getRequestInfo()
+	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
@@ -57,9 +59,9 @@ function getRequestInfo()
 		sendResultInfoAsJson( $retValue );
 	}
 
-	function returnWithInfo( $firstName, $lastName, $id )
+	function returnWithInfo( $cid, $id, $firstName, $lastName, $phone, $email )
 	{
-		$retValue = '{"id":' . $id . ',"first_name":"' . $first_name . '","last_name":"' . $last_name . '","phone":"'. $phone . '","email":"' . $email . '","error":""}';
+		$retValue = '{"id":' . $id . ',"cid":"' . $cid . ',"first_name":"' . $firstName . '","last_name":"' . $lastName . '","phone":"'. $phone . '","email":"' . $email . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 

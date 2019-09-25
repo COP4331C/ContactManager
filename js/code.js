34fe76f13d5810ed3307692601ab4aac82b5020b
anhtel94 <44963197+anhtel94@users.noreply.github.com>
2019-09-25 19:03:04 -0400

    Update code.js
    change false to true in doLogin

--- js/code.js
+++ js/code.js
@@ -39,7 +39,7 @@ function doLogin()
 // 	var url = urlBase + '/contacts.html';
 	
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
+	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{

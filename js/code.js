af7a21a3578c0286eef9bdceaa9d461d882eb041
anhtel94 <44963197+anhtel94@users.noreply.github.com>
2019-09-26 10:58:55 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -43,8 +43,8 @@ function doCreateAccount()
 	var last_Name = document.getElementById("lastName").value;
 	var address = document.getElementById("address").value;
 
-	//var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var jsonPayload = '{"first_Name" : "' + first_Name + '", "last_Name" : "' + last_Name + '", "phone" : "' + phone + '", "email" : "' + login + '", "password" : "' + password + '", "address" : "' + address + '"}';
+// 	var jsonPayload = '{"first_Name" : "' + first_Name + '", "last_Name" : "' + last_Name + '", "phone" : "' + phone + '", "email" : "' + login + '", "password" : "' + password + '", "address" : "' + address + '"}';
+	var jsonPayload = JSON.stringify({first_Name:first_Name, last_Name:last_Name, phone:phone, email:login, password:password, address:address});
 	var url = urlBase + '/LAMPAPI/create_account.' + extension;
 
 	var xhr = new XMLHttpRequest();

a6f243200cda1cc9ad543b6601d5cc1badf85147
Clyde <klabu123@gmail.com>
2019-09-28 13:40:17 -0400

    Implementing fetchContactList()

--- js/code.js
+++ js/code.js
@@ -144,12 +144,15 @@ function doLogin()
 
 				// Otherwise, we successfuly got a user from the database.
 				document.getElementById("loginResult").innerHTML = "Success";
+
 				// Reset the username and password just for cleanliness
 				document.getElementById("email").value = "";
 				document.getElementById("password").value = "";
 
 				// Go to contacts.html
 				window.location.replace(urlBase + "/contacts.html");
+
+				fetchContactList(userId);
 			}
 		}
 	}
@@ -172,6 +175,60 @@ function doLogout()
 	hideOrShow( "loginDiv", false);
 	hideOrShow("welcomeDiv", true);
 }
+
+// Refresh the table of contacts based on the userId given.
+function fetchContactList(userId)
+{
+	//   document.getElementById("contactRetrieveResult").innerHTML = "";
+	var jsonPayload = '{"userId" : "' + userId + '"}';
+  var url = urlBase + 'api/retrieveContacts.' + extension;
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+  try
+  {
+  	xhr.send(jsonPayload);
+
+    xhr.onreadystatechange = function()
+    {
+    	if (this.readyState == 4 && this.status == 200)
+      {
+      	var jsonObject = JSON.parse(xhr.responseText);
+        //contactId = jsonObject.tableId;
+        if(jsonObject.error.length > 0)
+			  {
+					// document.getElementById("contactSearchResult").innerHTML = "No contacts were found.";
+					return;
+				}
+
+        var i;
+				// runs i times, where i = number of ids found
+        for (i = 0; i < jsonObject.cid.length; i++)
+				{
+		    	var table = document.getElementById("contactTable");
+		      var tr = document.createElement("tr");
+		      s = jsonObject.userId[i];
+
+					tr.setAttribute("cid" + s);
+		      tr.innerHTML =
+					'<td>' + jsonObject.first_name[i] + '</td>' +
+		      '<td>' + jsonObject.last_name[i] + '</td>' +
+					'<td>' + jsonObject.address[i] + '</td>' +
+		      '<td>' + jsonObject.email[i] + '</td>' +
+		      '<td>' + jsonObject.phone[i] + '</td>';
+		      table.appendChild(tr);
+				}
+      }
+    };
+   }
+   catch(err)
+   {
+      document.getElementById("contactSearchResult").innerHTML = err.message;
+   }
+}
+
 
 function hideOrShow( elementId, showState )
 {

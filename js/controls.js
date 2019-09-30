b9979d50acc524fc86886a7b098a4ecafff2d488
Clyde <klabu123@gmail.com>
2019-09-30 19:35:55 -0400

    Fix assigning table row id = cid

--- js/controls.js
+++ js/controls.js
@@ -84,7 +84,7 @@ function fetchContactList(user_id)
 				{
 					// Reference to the current row
 					var newRow = tableRef.insertRow(tableRef.rows.length);
-					newRow.id = "1"
+					newRow.id = jsonObject[i].cid;
 
 					for (j = 0; j < numCells; j++)
 					{

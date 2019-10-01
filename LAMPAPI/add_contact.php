7cc1c7bee1f3cd55606021cc4ea51386fa8e3d5d
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2019-10-01 11:54:43 -0400

    Update add_contact.php

--- LAMPAPI/add_contact.php
+++ LAMPAPI/add_contact.php
@@ -20,18 +20,25 @@ $conn = new mysqli($servername, $uname, $pword, $dbname);
 
   //$sql ="INSERT INTO contact_list (id, first_name, last_name, phone, email) VALUES('34','TRIET', 'BRYAN', 1234567890, 'Toawesomeforthisproject@gmail.com' , 'NULL')";
   $sql = "INSERT INTO `contact_list` (id, first_name, last_name, phone, email, address) VALUES('" . $inData["id"] . "', '" . $inData["first_name"] . "', '" . $inData["last_name"] . "', '" . $inData["phone"] . "', '" . $inData["email"] . "', '" . $inData["address"] . "')";
+  $sql2 = "SELECT cid FROM contact_list where id='". $inData["id"] . "' and first_name = '" . $inData["first_name"] . "' and last_name = '" . $inData["last_name"] . "' and phone = '" . $inData["phone"] . "' and email = '" . $inData["email"] .  "' and address = '" . $inData["address"] . "'";
   if($conn->query($sql) === TRUE)
   {
-    echo "New record created successfully";
-    $id = $inData["id"];
-    $firstName = $inData["first_name"];
-    $lastName = $inData["last_name"];
-    $phone = $inData["phone"];
-    $email = $inData["email"];
-    $address = $inData["address"];
-    $conn->close();
+    $result = $conn->query($sql2);
+    if($result->num_rows > 0)
+    {
+      echo "New record created successfully";
+      $row = $result->fetch_assoc();
+      $cid = $row["cid"];
+      $id = $inData["id"];
+      $firstName = $inData["first_name"];
+      $lastName = $inData["last_name"];
+      $phone = $inData["phone"];
+      $email = $inData["email"];
+      $address = $inData["address"];
+      $conn->close();
+    }
     
-    returnWithInfo($id, $firstName, $lastName, $phone, $email, $address);
+    returnWithInfo($id,$cid, $firstName, $lastName, $phone, $email, $address);
     
   }
   else 
@@ -59,7 +66,7 @@ function returnWithError( $err )
 }
 function returnWithInfo( $id, $firstName, $lastName, $phone, $email, $address)
 {
-  $retValue = '{"id":"'. $id . '","first_name":"' . $firstName . '","last_name":"' . $lastName . '","phone":"'. $phone . '","email":"' . $email . '","address":"' . $address . '","error":""}';
+  $retValue = '{"id":"'. $id . '","cid":"'. $cid . '","first_name":"' . $firstName . '","last_name":"' . $lastName . '","phone":"'. $phone . '","email":"' . $email . '","address":"' . $address . '","error":""}';
   sendResultInfoAsJson( $retValue );
 }
 
